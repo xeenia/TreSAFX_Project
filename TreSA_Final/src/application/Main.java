e64934a501f932d39db88f81aaadf1875afc8ca4
Polyxeni Motsi <polyxenhmotsi@outlook.com.gr>
2021-12-08 21:01:01 +0200

    Basic UI for Search Engine

--- TreSA_Final/src/application/Main.java
+++ TreSA_Final/src/application/Main.java
@@ -2,6 +2,7 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
@@ -11,9 +12,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- TreSA_Final/src/module-info.java
+++ TreSA_Final/src/module-info.java
@@ -1,6 +1,7 @@
 module TreSA_Final {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
